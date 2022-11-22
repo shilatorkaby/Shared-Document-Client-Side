c82ad594c556086d4bfee577ec081fb1a40ab1da
shilatorkaby <shillat14@gmail.com>
2022-11-22 16:37:17 +0200

    add import file button

--- dist/edit.js
+++ dist/edit.js
@@ -81,3 +81,33 @@ function saveChanges() {
 
 
 
+
+
+
+function importTextFromFile() {
+
+	var inputFile = document.createElement('input');
+	inputFile.type = 'file';
+	inputFile.onchange = e => { 
+
+		// getting a hold of the file reference
+		var file = e.target.files[0]; 
+	 
+		// setting up the reader
+		var reader = new FileReader();
+		reader.readAsText(file,'UTF-8');
+	 
+		// here we tell the reader what to do when it's done reading...
+		reader.onload = readerEvent => {
+		   var content = readerEvent.target.result; // this is the content!
+		   console.log( content );
+		   document.getElementById("content").innerHTML = content;
+
+		}
+	 
+	 }
+	 inputFile.click();
+
+	}
+
+ 
